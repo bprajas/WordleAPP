70cf63d0fac02432ff9564ebe49d60af533aaac5
Prajas Bharadwaj <prajas.bharadwaj@gmail.com>
2025-06-28 16:25:44 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,94 @@
+import streamlit as st
+import itertools
+import requests
+import numpy as np
+import pandas as pd
+
+@st.cache_data
+
+def load_wordle_data():
+    url = "https://web.archive.org/web/20220101000805js_/https://www.powerlanguage.co.uk/wordle/main.db1931a8.js"
+    response = requests.get(url)
+    data = response.text
+    wordle_answers = data.split("var Aa=[")[-1].split("]")[0].replace('"', "").upper().split(",")
+    valid_entries = data.split(",La=[")[-1].split("]")[0].replace('"', "").upper().split(",")
+    valid_entries = sorted(list(set(wordle_answers + valid_entries)))
+    wordle_answers = sorted(wordle_answers)
+    return wordle_answers, valid_entries
+
+def indices(arr, value):
+    return [ind for ind, x in enumerate(arr) if x == value]
+
+def color_sequence(guess, answer, emoji=True):
+    guess = list(guess)
+    answer = list(answer)
+    colors = ["W"] * len(answer)
+    for ind, (g, a) in enumerate(zip(guess, answer)):
+        if g == a:
+            colors[ind] = "G"
+            guess[ind] = ""
+            answer[ind] = ""
+    for ind, (g, a) in enumerate(zip(guess, answer)):
+        if g != a and g in answer and g != "":
+            g_indices = indices(guess, g)
+            a_indices = indices(answer, g)
+            if not any(i in g_indices for i in a_indices):
+                colors[ind] = "Y"
+                answer[answer.index(g)] = ""
+    colors = "".join(colors)
+    if emoji:
+        colors = colors.replace("G", "🟩").replace("Y", "🟨").replace("W", "⬜")
+    return colors
+
+def get_words_using_sequence(input_word, sequence, word_list):
+    return [word for word in word_list if color_sequence(input_word, word) == sequence]
+
+def count_of_combos(guess, answers, probability=True):
+    results = {}
+    increment = 1 / len(answers) if probability else 1
+    for ans in answers:
+        seq = color_sequence(guess, ans)
+        results[seq] = results.get(seq, 0) + increment
+    return results
+
+def entropy_from_combos(combos):
+    return sum(p * (-np.log2(p)) for p in combos.values())
+
+def entropy_df(input_words, possible_answers):
+    data = []
+    for word in input_words:
+        combos = count_of_combos(word, possible_answers)
+        entropy = entropy_from_combos(combos)
+        data.append((word, entropy))
+    df = pd.DataFrame(data, columns=["WORD", "ENTROPY"])
+    df = df.sort_values("ENTROPY", ascending=False).set_index("WORD")
+    return df
+
+# Streamlit UI
+st.title("Wordle Entropy Maximizer")
+
+wordle_answers, valid_words = load_wordle_data()
+
+cols = st.columns(4)
+input_words = []
+patterns = []
+
+with st.form("wordle_form"):
+    for i in range(4):
+        with cols[i]:
+            word = st.text_input(f"Guess {i+1}", "", max_chars=5).upper()
+            pattern = st.text_input(f"Pattern {i+1} (G/Y/W)", "", max_chars=5).upper()
+            if word and pattern:
+                pattern = pattern.replace("G", "🟩").replace("Y", "🟨").replace("W", "⬜")
+                input_words.append(word)
+                patterns.append(pattern)
+    submitted = st.form_submit_button("Calculate Best Next Guesses")
+
+if submitted:
+    current_list = wordle_answers
+    for word, pat in zip(input_words, patterns):
+        current_list = get_words_using_sequence(word, pat, current_list)
+
+    df_entropy = entropy_df(current_list, wordle_answers)
+    st.subheader("Top 15 Words by Entropy")
+    st.dataframe(df_entropy.head(15))
